9961cda91c623691e1fb5a755bd494815ca7f5c4
Roy Choo <rchoo@paypal.com>
2012-06-20 16:02:59 +0800

    added hero unit

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 	<div class="row-fluid">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class='span12'>
-			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<div <?php post_class('hero-unit') ?> id="post-<?php the_ID(); ?>">
 				
 				<h2><?php the_title(); ?></h2>
 				
