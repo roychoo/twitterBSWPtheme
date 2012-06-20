c6d110634a5c063c5a6bbc886b1f5fe122cb28c2
Roy Choo <rchoo@paypal.com>
2012-06-20 11:16:46 +0800

    initial comment

---
+++ functions.php
@@ -0,0 +1,85 @@
+<?php
+	
+	// Add RSS links to <head> section
+	automatic_feed_links();
+
+    //Load BootStrap Responsive CSS /css/Bootstrap-responsive.css
+    function wptuts_styles_with_the_lot()  
+    {  
+        // Register the style like this for a plugin:  
+        //wp_register_style( 'custom-style', plugins_url( '/css/Bootstrap-responsive.css', __FILE__ ), array(), '20120208', 'all' );  
+        // or  
+        // Register the style like this for a theme:  
+        wp_register_style( 'custom-style', get_template_directory_uri() . '/css/Bootstrap-responsive.css', array(), '20120208', 'all' );  
+      
+        // For either a plugin or a theme, you can then enqueue the style:  
+        wp_enqueue_style( 'custom-style' );  
+    }  
+    add_action( 'wp_enqueue_scripts', 'wptuts_styles_with_the_lot' ); 
+	
+	// Load jQuery
+	if ( !is_admin() ) {
+	   wp_deregister_script('jquery');
+	  
+	  // wp_enqueue_script('jquery');
+	}
+	
+	// Clean up the <head>
+	function removeHeadLinks() {
+    	remove_action('wp_head', 'rsd_link');
+    	remove_action('wp_head', 'wlwmanifest_link');
+    }
+    add_action('init', 'removeHeadLinks');
+    remove_action('wp_head', 'wp_generator');
+    
+    if (function_exists('register_sidebar')) {
+    	register_sidebar(array(
+    		'name' => 'Sidebar Widgets',
+    		'id'   => 'sidebar-widgets',
+    		'description'   => 'These are widgets for the sidebar.',
+    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    		'after_widget'  => '</div>',
+    		'before_title'  => '<h2>',
+    		'after_title'   => '</h2>'
+    	));
+    }
+
+    function add_scripts_after_footer() {
+        // Register the script like this for a plugin:  
+        wp_register_script( 'custom-script', plugins_url( '/js/custom-script.js', __FILE__ ) );  
+        // or  
+        // Register the script like this for a theme:  
+        wp_register_script('jquery', ("https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"), false);
+        wp_register_script( 'bootstrap-alert', get_template_directory_uri() . '/js/bootstrap-alert.js' );  
+        wp_register_script( 'bootstrap-button', get_template_directory_uri() . '/js/bootstrap-button.js' );  
+        wp_register_script( 'bootstrap-carousel', get_template_directory_uri() . '/js/bootstrap-carousel.js' );  
+        wp_register_script( 'bootstrap-collapse', get_template_directory_uri() . '/js/bootstrap-collapse.js' );  
+        wp_register_script( 'bootstrap-dropdown', get_template_directory_uri() . '/js/bootstrap-dropdown.js' );  
+        wp_register_script( 'bootstrap-modal', get_template_directory_uri() . '/js/bootstrap-modal.js' );  
+        wp_register_script( 'bootstrap-popover', get_template_directory_uri() . '/js/bootstrap-popover.js' );  
+        wp_register_script( 'bootstrap-scrollspy', get_template_directory_uri() . '/js/bootstrap-scrollspy.js' );  
+        wp_register_script( 'bootstrap-tab', get_template_directory_uri() . '/js/bootstrap-tab.js' );  
+        wp_register_script( 'bootstrap-tooltip', get_template_directory_uri() . '/js/bootstrap-tooltip.js' );  
+        wp_register_script( 'bootstrap-transition', get_template_directory_uri() . '/js/bootstrap-transition.js' );  
+        wp_register_script( 'bootstrap-tyeahead', get_template_directory_uri() . '/js/bootstrap-tyeahead.js' );  
+      
+     
+      
+        // For either a plugin or a theme, you can then enqueue the script:  
+        wp_enqueue_script('jquery');
+        wp_enqueue_script( 'bootstrap-alert' );
+        wp_enqueue_script( 'bootstrap-button' );
+        wp_enqueue_script( 'bootstrap-carousel' );
+        wp_enqueue_script( 'bootstrap-dropdown' );
+        wp_enqueue_script( 'bootstrap-modal' );
+        wp_enqueue_script( 'bootstrap-popover' );
+        wp_enqueue_script( 'bootstrap-scrollspy' );
+        wp_enqueue_script( 'bootstrap-tab' );
+        wp_enqueue_script( 'bootstrap-tooltip' );
+        wp_enqueue_script( 'bootstrap-transition' );
+        wp_enqueue_script( 'bootstrap-tyeahead' );
+
+    }
+    add_action('wp_footer', 'add_scripts_after_footer');
+
+?>
---
+++ header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+  	<meta charset=<?php bloginfo('charset'); ?>/>
+	<title>
+	   <?php
+	      if (function_exists('is_tag') && is_tag()) {
+	         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+	      elseif (is_archive()) {
+	         wp_title(''); echo ' Archive - '; }
+	      elseif (is_search()) {
+	         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+	      elseif (!(is_404()) && (is_single()) || (is_page())) {
+	         wp_title(''); echo ' - '; }
+	      elseif (is_404()) {
+	         echo 'Not Found - '; }
+	      if (is_home()) {
+	         bloginfo('name'); echo ' - '; bloginfo('description'); }
+	      else {
+	          bloginfo('name'); }
+	      if ($paged>1) {
+	         echo ' - page '. $paged; }
+	   ?>
+	</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+	<?php if (is_search()) { ?>
+	   <meta name="robots" content="noindex, nofollow" /> 
+	<?php } ?>
+	
+	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/images/favicon.ico" />
+	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+	
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+
+	<?php wp_head(); ?>
+	
+</head>
+
+<body <?php body_class(); ?>>
+
+	<div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>
+          <div class="btn-group pull-right">
+            <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+              <i class="icon-user"></i> Username
+              <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Profile</a></li>
+              <li class="divider"></li>
+              <li><a href="#">Sign Out</a></li>
+            </ul>
+          </div>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+     <div class="container-fluid">
+
