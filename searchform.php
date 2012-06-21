6f480bc8eabfd9aba236a5572a6d05950e18335c
Roy Choo <rchoo@paypal.com>
2012-06-21 09:55:48 +0800

    remove the label "Search for"

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
     <div id="inner">
-        <label for="s" class="screen-reader-text">Search for:</label>
+        
         <span><input type="text" id="text" name="s" value="" /></span>
         
         <input type="submit" value="Search" id="searchsubmit" />
