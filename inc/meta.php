03aecb9f773f5440e3a24273bbfd035ebfd6d8e8
unknown <roychoo@hotmail.com>
2012-06-22 00:06:21 +0800

    add a cal icon

--- inc/meta.php
+++ inc/meta.php
@@ -1,5 +1,5 @@
 <div class="meta">
-	<em>Posted on:</em> <?php the_time('F j, Y') ?>
+	<img src="http://107.21.120.115/wordpress/wp-content/uploads/2012/06/calendar.png"/> <?php the_time('F j, Y') ?>
 	<em>by</em> <?php the_author() ?>
 	<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
 </div>
