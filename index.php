00a021eef676377d18f8ab721bc895f1762b7d0b
unknown <roychoo@hotmail.com>
2012-06-22 01:02:26 +0800

    overwriting the social icon styles

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 					<?php the_content(); ?>
 				</div>
 				<!-- AddThis Button BEGIN -->
-				<div class="addthis_toolbox addthis_default_style ">
+				<div class="addthis_toolbox addthis_default_style">
 					<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
 					<a class="addthis_button_tweet"></a>
 					<a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
