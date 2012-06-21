9378ecca85184fca4e242fe107663b51ef1dfa5c
unknown <roychoo@hotmail.com>
2012-06-22 01:11:32 +0800

    .

--- inc/meta.php
+++ inc/meta.php
@@ -1,7 +1,7 @@
 <div class="meta">
 	<img src="http://107.21.120.115/wordpress/wp-content/uploads/2012/06/calendar.png"/> <?php the_time('F j, Y') ?>
 	<em>by</em> <?php the_author() ?>
-	<div>
+	<p>
 		<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
-	<div>
+	</p>
 </div>
